6422415dafacbe00492577c0057262b931d36e3b
Paulo Lemes <paulo-lemes@live.com>
2024-07-18 23:49:52 -0300

    Added vercel configs

--- src/server.ts
+++ src/server.ts
@@ -1,33 +1,32 @@
-import fastify from "fastify";
 import cors from "@fastify/cors";
-import { createTrip } from "./routes/create-trip";
+import fastify from "fastify";
 import {
   serializerCompiler,
   validatorCompiler,
 } from "fastify-type-provider-zod";
-import { confirmTrip } from "./routes/confirm-trip";
-import { confirmParticipants } from "./routes/confirm-participant";
-import { createActivity } from "./routes/create-activity";
-import { getActivities } from "./routes/get-activities";
-import { createLink } from "./routes/create-link";
-import { getLinks } from "./routes/get-links";
-import { getParticipants } from "./routes/get-participants";
-import { createInvite } from "./routes/create-invite";
-import { updateTrip } from "./routes/update-trip";
-import { getTripDetails } from "./routes/get-trip-details";
-import { getParticipant } from "./routes/get-participant";
-import { errorHandler } from "./error-handler";
 import { env } from "./env";
+import { errorHandler } from "./error-handler";
 import { changeNameParticipant } from "./routes/change-name-participant";
+import { confirmParticipants } from "./routes/confirm-participant";
+import { confirmTrip } from "./routes/confirm-trip";
+import { createActivity } from "./routes/create-activity";
 import { createInvites } from "./routes/create-invites";
-import { deleteLink } from "./routes/delete-link";
+import { createLink } from "./routes/create-link";
+import { createTrip } from "./routes/create-trip";
 import { deleteActivity } from "./routes/delete-activity";
+import { deleteLink } from "./routes/delete-link";
 import { deleteParticipant } from "./routes/delete-participant";
+import { getActivities } from "./routes/get-activities";
+import { getLinks } from "./routes/get-links";
+import { getParticipant } from "./routes/get-participant";
+import { getParticipants } from "./routes/get-participants";
+import { getTripDetails } from "./routes/get-trip-details";
+import { updateTrip } from "./routes/update-trip";
 
 const app = fastify();
 
 app.register(cors, {
-  origin: "*",
+  origin: env.WEB_BASE_URL,
 });
 
 app.setValidatorCompiler(validatorCompiler);
